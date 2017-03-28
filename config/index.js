b00476d44414dae26b98dde598d45548cf83282f
ckwu <neco.wu@ichunshen.com>
2017-03-28 14:45:36 +0800

    remove unsafe

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@ module.exports = {
   version: '1.0',
   db_url: 'mongodb://localhost:27017/comment',
   email: {
-    user:    "postmaster@wuchengkai.com", 
-    password:"L96118045hqj", 
+    user:    "", 
+    password:"", 
     host:    "smtp.mxhichina.com", 
     ssl:     true,
     base:    'https://www.wuchengkai.com/'
